c37ccba82204998991bd94189333430841747372
mo.elkady <mo.elkady@toptal.com>
2024-02-04 08:31:56 +0200

    [after time] showing avarage rating

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,19 +1,18 @@
-# app/controllers/movies_controller.rb
-
 class MoviesController < ApplicationController
   def index
-    @movies = if params[:search].present?
-                Movie.joins(:actors)
-                     .where('actors.name LIKE ?', "%#{params[:search]}%")
-                     .includes(:reviews, :location, :director)
-                     .distinct
-              else
-                Movie.all
-              end
+    base_query = if params[:search].present?
+                   Movie.joins(:actors)
+                        .where('actors.name LIKE ?', "%#{params[:search]}%")
+                        .distinct
+                 else
+                   Movie.all
+                 end
 
-    @movies = @movies.left_joins(:reviews)
-                     .group('movies.id')
-                     .order(Arel.sql('AVG(reviews.stars) DESC NULLS LAST'))
-                     .includes(:reviews, :location, :director)
+    @movies = base_query
+                .left_joins(:reviews)
+                .select('movies.*', 'AVG(reviews.stars) AS average_rating')
+                .group('movies.id')
+                .order(Arel.sql('average_rating DESC NULLS LAST'))
+                .includes(:reviews, :location, :director)
   end
 end
